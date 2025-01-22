199612f21ef01200c4ead008c21552b14dfa14fc
NoahMattheis <noahwkm@gmail.com>
2025-01-22 11:09:07 -0500

    First website made using RMarkdown

---
+++ FirstMarkdown.R
@@ -0,0 +1,38 @@
+## ----setup, include=FALSE----------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ----eval=TRUE, echo=FALSE---------------------------------------------------------------
+# r code can be used here! 
+
+print(runif(10))
+plot(runif(10), runif(10))
+
+
+
+
+## ----eval=TRUE, echo=TRUE----------------------------------------------------------------
+# r code can be used here! 
+
+print(runif(10))
+plot(runif(10), runif(10))
+
+
+
+
+## ----eval=FALSE--------------------------------------------------------------------------
+## # r code can be used here!
+## 
+## print(runif(10))
+## plot(runif(10), runif(10))
+## 
+## 
+
+
+## ----------------------------------------------------------------------------------------
+#Here is a new chunk of code, distant from the first one in our document.
+
+z <- 1:10
+print(z)
+#end of second chunk
+
